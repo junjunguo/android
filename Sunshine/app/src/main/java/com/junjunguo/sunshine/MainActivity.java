c352cad23edc02116150b74c5a1311269489680e
Guo Junjun <guojunjun@gmail.com>
2014-12-20 17:22:46 +0100

    AsyncTask

--- Sunshine/app/src/main/java/com/junjunguo/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/junjunguo/sunshine/MainActivity.java
@@ -1,20 +1,9 @@
 package com.junjunguo.sunshine;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -25,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -53,56 +42,5 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     * 1<p/>
-     * a fragment is a modular container with activity
-     * <p/>
-     * fragment_main: res/layout/fragment_main.xml
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            List<String> alist = new ArrayList<>();
-            alist.add("Today - Sunny - 88/63");
-            alist.add("Tomorrow - Sunny - 88/63");
-            alist.add("Weds - Sunny - 88/63");
-            alist.add("Thurs - Sunny - 88/63");
-            alist.add("Fri - Sunny - 88/63");
-            alist.add("Sat - Sunny - 88/63");
-
-            /*
-             *  ArrayAdapter<String> : 
-             *  Parameters:
-             *      Context:
-             *              contained global information about app environment*
-             *      ID of list item layout
-             *              *
-             *      ID of text view
-             *      list of data
-             */
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
-                    // the current context
-                    getActivity(),
-                    // id of list item layout
-                    R.layout.list_item_forecast, 
-                    // id of the textview to populate
-                    R.id.list_item_forecast_textview, 
-                    // data
-                    alist);
-            // get a reference to the ListView, and attach this adapter to listview
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(arrayAdapter);
-            
-                    
-            return rootView;
-        }
-    }
+//    ForecastFragment fview = new ForecastFragment();
 }
