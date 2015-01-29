decab1eb4b88bde04f8da773d3c5fa041ea9a396
Guo Junjun <guojunjun@gmail.com>
2015-01-29 13:34:59 +0100

    How to get acceleration & how it works

---
+++ Acceleration/app/src/main/java/com/junjunguo/acceleration/MainActivity.java
@@ -0,0 +1,115 @@
+package com.junjunguo.acceleration;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity implements SensorEventListener {
+    private float mLastX, mLastY, mLastZ;
+    private boolean mInitialized;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private final float NOISE = (float) 0.1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mInitialized = false;
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override public void onSensorChanged(SensorEvent event) {
+        TextView tvX = (TextView) findViewById(R.id.x_axis);
+        TextView tvY = (TextView) findViewById(R.id.y_axis);
+        TextView tvZ = (TextView) findViewById(R.id.z_axis);
+        ImageView iv = (ImageView) findViewById(R.id.image);
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+        if (!mInitialized) {
+            mLastX = x;
+            mLastY = y;
+            mLastZ = z;
+            tvX.setText("0.0");
+            tvY.setText("0.0");
+            tvZ.setText("0.0");
+            mInitialized = true;
+        } else {
+            float deltaX = Math.abs(mLastX - x);
+            float deltaY = Math.abs(mLastY - y);
+            float deltaZ = Math.abs(mLastZ - z);
+            if (deltaX < NOISE)
+                deltaX = (float) 0.0;
+            if (deltaY < NOISE)
+                deltaY = (float) 0.0;
+            if (deltaZ < NOISE)
+                deltaZ = (float) 0.0;
+            mLastX = x;
+            mLastY = y;
+            mLastZ = z;
+            tvX.setText(Float.toString(deltaX));
+            tvY.setText(Float.toString(deltaY));
+            tvZ.setText(Float.toString(deltaZ));
+            iv.setVisibility(View.VISIBLE);
+            if (deltaX > deltaY) {
+                iv.setImageResource(R.drawable.leftright);
+            } else if (deltaY > deltaX) {
+                iv.setImageResource(R.drawable.updown);
+            } else {
+                iv.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    @Override public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
