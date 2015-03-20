65e38285e8fca2e08bc2e8c9aa503d607aae746f
junjunguo <guojunjun@gmail.com>
2015-03-20 17:05:53 +0100

    alert dialog builder: dialog view

---
+++ 2015/AlertDialogBuilder/app/src/main/java/com/junjunguo/alertdialogbuilder/MainActivity.java
@@ -0,0 +1,64 @@
+package com.junjunguo.alertdialogbuilder;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnAlertDialogClicked(View v) {
+        // custom dialog
+        Toast.makeText(this, "show alert clicked--------", Toast.LENGTH_SHORT).show();
+
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.custom_layout);
+        dialog.setTitle("Alert Dialog View");
+
+        TextView textView = (TextView) dialog.findViewById(R.id.custom_text_view);
+        textView.setText("Alert Dialog view : imageView+textView+Button");
+
+        Button btnExit = (Button) dialog.findViewById(R.id.btnExit);
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        // show dialog on screen
+        dialog.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
