5ec03a6ab13fcd97dd9e0bb967ed5245b1400a52
Junjun Guo <guojunjun@gmail.com>
2015-10-16 10:59:23 +0200

    add image directly to image adapter ...

--- 2015/SqliteImage/app/src/main/java/com/junjunguo/sqliteimage/MainActivity.java
+++ 2015/SqliteImage/app/src/main/java/com/junjunguo/sqliteimage/MainActivity.java
@@ -13,7 +13,9 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.ListView;
+
 import com.google.gson.Gson;
+
 import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
@@ -26,8 +28,7 @@ public class MainActivity extends ActionBarActivity {
     private static final int REQUEST_IMAGE_CAPTURE = 2;
     private DAOdb daOdb;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -63,18 +64,16 @@ public class MainActivity extends ActionBarActivity {
                 dialog.dismiss();
             }
         });
-        dialog.findViewById(R.id.btnChoosePath)
-                .setOnClickListener(new View.OnClickListener() {
-                    @Override public void onClick(View v) {
-                        activeGallery();
-                    }
-                });
-        dialog.findViewById(R.id.btnTakePhoto)
-                .setOnClickListener(new View.OnClickListener() {
-                    @Override public void onClick(View v) {
-                        activeTakePhoto();
-                    }
-                });
+        dialog.findViewById(R.id.btnChoosePath).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                activeGallery();
+            }
+        });
+        dialog.findViewById(R.id.btnTakePhoto).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                activeTakePhoto();
+            }
+        });
 
         // show dialog on screen
         dialog.show();
@@ -89,11 +88,8 @@ public class MainActivity extends ActionBarActivity {
             String fileName = "temp.jpg";
             ContentValues values = new ContentValues();
             values.put(MediaStore.Images.Media.TITLE, fileName);
-            mCapturedImageURI = getContentResolver()
-                    .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                            values);
-            takePictureIntent
-                    .putExtra(MediaStore.EXTRA_OUTPUT, mCapturedImageURI);
+            mCapturedImageURI = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCapturedImageURI);
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
@@ -102,13 +98,11 @@ public class MainActivity extends ActionBarActivity {
      * to gallery
      */
     private void activeGallery() {
-        Intent intent = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent, RESULT_LOAD_IMAGE);
     }
 
-    @Override protected void onActivityResult(int requestCode, int resultCode,
-            Intent data) {
+    @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
             case RESULT_LOAD_IMAGE:
@@ -116,60 +110,50 @@ public class MainActivity extends ActionBarActivity {
                         resultCode == RESULT_OK && null != data) {
                     Uri selectedImage = data.getData();
                     String[] filePathColumn = {MediaStore.Images.Media.DATA};
-                    Cursor cursor = getContentResolver()
-                            .query(selectedImage, filePathColumn, null, null,
-                                    null);
+                    Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
                     cursor.moveToFirst();
                     int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
                     String picturePath = cursor.getString(columnIndex);
                     cursor.close();
                     MyImage image = new MyImage();
                     image.setTitle("Test");
-                    image.setDescription(
-                            "test choose a photo from gallery and add it to " +
-                                    "list view");
+                    image.setDescription("test choose a photo from gallery and add it to " + "list view");
                     image.setDatetime(System.currentTimeMillis());
                     image.setPath(picturePath);
-                    images.add(image);
+                    //                    images.add(image);//notifyDataSetChanged does not work well sometimes
+                    imageAdapter.add(image);
                     daOdb.addImage(image);
                 }
             case REQUEST_IMAGE_CAPTURE:
-                if (requestCode == REQUEST_IMAGE_CAPTURE &&
-                        resultCode == RESULT_OK) {
+                if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
                     String[] projection = {MediaStore.Images.Media.DATA};
-                    Cursor cursor =
-                            managedQuery(mCapturedImageURI, projection, null,
-                                    null, null);
-                    int column_index_data = cursor.getColumnIndexOrThrow(
-                            MediaStore.Images.Media.DATA);
+                    Cursor cursor = managedQuery(mCapturedImageURI, projection, null, null, null);
+                    int column_index_data = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
                     cursor.moveToFirst();
                     String picturePath = cursor.getString(column_index_data);
                     MyImage image = new MyImage();
                     image.setTitle("Test");
-                    image.setDescription(
-                            "test take a photo and add it to list view");
+                    image.setDescription("test take a photo and add it to list view");
                     image.setDatetime(System.currentTimeMillis());
                     image.setPath(picturePath);
-                    images.add(image);
+                    imageAdapter.add(image);
+                    //                    images.add(image);
                     daOdb.addImage(image);
                 }
         }
     }
 
     /**
-     * item clicked listener used to implement the react action when an item is
-     * clicked.
+     * item clicked listener used to implement the react action when an item is clicked.
      *
      * @param listView
      */
     private void addItemClickListener(final ListView listView) {
         listView.setOnItemClickListener(new OnItemClickListener() {
-            @Override public void onItemClick(AdapterView<?> parent, View view,
-                    int position, long id) {
+            @Override public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                 MyImage image = (MyImage) listView.getItemAtPosition(position);
-                Intent intent =
-                        new Intent(getBaseContext(), DisplayImage.class);
+                Intent intent = new Intent(getBaseContext(), DisplayImage.class);
                 intent.putExtra("IMAGE", (new Gson()).toJson(image));
                 startActivity(intent);
             }
@@ -179,8 +163,7 @@ public class MainActivity extends ActionBarActivity {
     @Override protected void onSaveInstanceState(Bundle outState) {
         // Save the user's current game state
         if (mCapturedImageURI != null) {
-            outState.putString("mCapturedImageURI",
-                    mCapturedImageURI.toString());
+            outState.putString("mCapturedImageURI", mCapturedImageURI.toString());
         }
         // Always call the superclass so it can save the view hierarchy state
         super.onSaveInstanceState(outState);
@@ -192,8 +175,7 @@ public class MainActivity extends ActionBarActivity {
 
         // Restore state members from saved instance
         if (savedInstanceState.containsKey("mCapturedImageURI")) {
-            mCapturedImageURI = Uri.parse(
-                    savedInstanceState.getString("mCapturedImageURI"));
+            mCapturedImageURI = Uri.parse(savedInstanceState.getString("mCapturedImageURI"));
         }
     }
 }
